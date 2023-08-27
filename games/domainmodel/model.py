fa95b2f5ee2594731157c572c61d2c7dbc443693
Mike Li <xli751@aucklanduni.ac.nz>
2023-08-27 19:26:57 +1200

    hello

--- games/domainmodel/model.py
+++ games/domainmodel/model.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 ```hello here is the codeasdasd
+'''hello this mike'''
 class Publisher:
     def __init__(self, publisher_name: str):
         if publisher_name == "" or type(publisher_name) is not str:
