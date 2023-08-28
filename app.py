85a77028fc91e597823d886a06ae163c9b33a504
Mike Li <xli751@aucklanduni.ac.nz>
2023-08-28 17:46:50 +1200

    some how Julia's page can't be run on flask, so I make some big changes on the whole page, but there's still bugs remain, but at least most of things can be runs on flask . For codes on app.py file, I put them into games\__init__ file, so app.py is no longer bu used.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+'''from flask import Flask, render_template
 
 from games.domainmodel import model
 from games.adapters.datareader import csvdatareader
--- games/__init__.py
+++ games/__init__.py
@@ -1,13 +1,14 @@
 """Initialize Flask app."""
-
+import games.adapters.datareader.csvdatareader
 from flask import Flask, render_template
 
+from games.adapters.datareader.csvdatareader import GameFileCSVReader
 # TODO: Access to the games should be implemented via the repository pattern and using blueprints, so this can not
 #  stay here!
 from games.domainmodel.model import Game
 
 
-# TODO: Access to the games should be implemented via the repository pattern and using blueprints, so this can not
+'''# TODO: Access to the games should be implemented via the repository pattern and using blueprints, so this can not
 #  stay here!
 def create_some_game():
     some_game = Game(1, "Call of Duty® 4: Modern Warfare®")
@@ -16,7 +17,7 @@ def create_some_game():
     some_game.description = "The new action-thriller from the award-winning team at Infinity Ward, the creators of " \
                             "the Call of Duty® series, delivers the most intense and cinematic action experience ever. "
     some_game.image_url = "https://cdn.akamai.steamstatic.com/steam/apps/7940/header.jpg?t=1646762118"
-    return some_game
+    return some_game'''
 
 
 def create_app():
@@ -26,9 +27,68 @@ def create_app():
     app = Flask(__name__)
 
     @app.route('/')
-    def home():
-        some_game = create_some_game()
+    #def home():
+        #some_game = create_some_game()
         # Use Jinja to customize a predefined html page rendering the layout for showing a single game.
-        return render_template('gameDescription.html', game=some_game)
+    def show_layout():
+        #return render_template('gameDescription.html', game=some_game)
+        return render_template('layout.html')
+
+    @app.route('/gamesDescription')
+    def show_gamedesc():
+        return render_template('gameDescription.html')
+
+    @app.route('/games')
+    def show_games():
+        games_file_name = "static/games.csv"
+        reader = GameFileCSVReader(games_file_name)
+        reader.read_csv_file()
+        listOfGames = [reader.dataset_of_games]
+
+        for index in range(0, reader.get_unique_games_count()):
+            game = listOfGames[index]
+            try:
+                Gamepart = {
+                    'name': 'game.title',
+                    'price': 'game.price',
+                    'image': 'game.image_url',
+                    'publishers': 'game.publisher',
+                    'date': 'game.release_date',
+                    'genres': 'game.genres',
+                    'reviews': 'len(game.reviews)'
+                }
+                listOfGames.append(Gamepart)
+            finally:
+                pass
+
+        '''listOfGamesExample = [{
+            'name': 'Oxygen Not Included',
+            'price': '$24.99',
+            'image': "https://cdn.akamai.steamstatic.com/steam/apps/457140/header_alt_assets_6.jpg?t=1654189805",
+            'publishers': 'Klei Entertainment',
+            'date': 'Jul 30, 2019',
+            'genres': 'Indie,Simulation',
+            'reviews': '0'
+        },
+            {
+                'name': 'Sally Face - Episode One',
+                'price': '$2.99',
+                'image': "https://cdn.akamai.steamstatic.com/steam/apps/541570/header.jpg?t=1619036403",
+                'publishers': 'Portable Moose',
+                'date': 'Dec 14, 2016',
+                'genres': 'Adventure,Indie',
+                'reviews': '5'
+            },
+            {
+                'name': 'Super Meat Boy',
+                'price': '$14.99',
+                'image': "https://cdn.akamai.steamstatic.com/steam/apps/40800/header.jpg?t=1638306971",
+                'publishers': 'Unknown',
+                'date': 'Nov 30, 2010',
+                'genres': 'Indie',
+                'reviews': '0'
+            }]'''
+
+        return render_template('games.html', listOfGames=listOfGames)
 
     return app
