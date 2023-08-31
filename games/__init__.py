c84bf04cb77c490183956d2141a8843195b1f847
Luke Lieynard Capinpin Dizon <140481720+SkywalkerL235@users.noreply.github.com>
2023-08-31 22:57:56 +1200

    These needs to be edited so they work as needed for the functions we make on the create_app

--- games/__init__.py
+++ games/__init__.py
@@ -73,7 +73,17 @@ def create_app(repository=repository):
     def filter_by_genre(selected_genre):
         filtered_list = repository.filter_by_genre(selected_genre)
         unique_genres = repository.get_unique_genres()
-        return render_template('filtered_games.html', filtered_games=filtered_list, selected_genre=selected_genre, unique_genres=unique_genres)
+        page = request.args.get('page', default=1, type=int)  # Get the page number from the query parameter
+        per_page = 20
+        number_of_total_games = len(filtered_list)
+
+        start_idx = (page - 1) * per_page
+        end_idx = start_idx + per_page
+        games_subset = filtered_list[start_idx:end_idx]
+        return render_template('filtered_games.html', filtered_games=games_subset, selected_genre=selected_genre, unique_genres=unique_genres, page=page,
+            per_page=per_page,
+            total_number_games=number_of_total_games
+        )
 
     return app
 
