30607324fb6d79546778ed9fc62cb5e3a70cd2fb
Mike Li <xli751@aucklanduni.ac.nz>
2023-09-01 21:52:24 +1200

    some more unit test

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -11,8 +11,10 @@ def client():
         yield client
 
 def test_service_returns_existing_game(client):
-    response = client.get('/game/435790')
-    assert response.status_code == 200
+    response1 = client.get('/game/435790')
+    response2 = client.get('/game/1671200')
+    assert response1.status_code == 200
+    assert response2.status_code == 200
 
 def test_service_retrieves_correct_number_of_games(client):
     response = client.get('/games')
@@ -27,24 +29,32 @@ def test_paginated_games(client):
     assert response3.status_code == 200
 
 def test_filter_games_by_genre(client):
-    response = client.get('/filtered_games/Action')
-    assert response.status_code == 200
+    response1 = client.get('/filtered_games/Action')
+    response2 = client.get('/filtered_games/Casual')
+    response3 = client.get('/filtered_games/RPG')
+    assert response1.status_code == 200
+    assert response2.status_code == 200
+    assert response3.status_code == 200
 
 def test_search_games_by_genre(client):
     response = client.get('/g_search')
     assert response.status_code == 200
 
 def test_search_games_by_name(client):
-    response = client.post('/n_search', data={'search': '10 Second Ninja X'})
-    assert response.status_code == 200
+    response1 = client.post('/n_search', data={'search': '10 Second Ninja X'})
+    response2 = client.post('/n_search', data={'search': 'Super Meat Boy'})
+    assert response1.status_code == 200
+    assert response2.status_code == 200
 
 def test_search_games_by_non_existing_name(client):
     response = client.post('/n_search', data={'search': '#%$@*'})
     assert response.status_code == 200
 
 def test_search_games_by_publisher(client):
-    response = client.post('/p_search', data={'search': '8floor'})
-    assert response.status_code == 200
+    response1 = client.post('/p_search', data={'search': '8floor'})
+    response2 = client.post('/p_search', data={'search': 'Stardock'})
+    assert response1.status_code == 200
+    assert response2.status_code == 200
 
 def test_search_games_by_non_existing_publisher(client):
     response = client.post('/p_search', data={'search': 'Non-existing Publisher'})
