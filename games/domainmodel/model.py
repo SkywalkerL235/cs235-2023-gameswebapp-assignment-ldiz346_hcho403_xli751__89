361f389db9cab09b0834a40101c14a279b30de3c
Ha-Eun Choi <hcho403@aucklanduni.ac.nz>
2023-09-22 23:53:44 +1200

    Profile set-up

--- games/domainmodel/model.py
+++ games/domainmodel/model.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
-'''hello here is the codeasdasd'''
-'''hello this mike'''
+
 class Publisher:
     def __init__(self, publisher_name: str):
         if publisher_name == "" or type(publisher_name) is not str:
